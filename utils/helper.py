3b94d5dc440976ac98db73d278edda35bc776a56
Oᴠᴇʀ ❖ Sᴛʀɪᴘᴇ <120897603+OverStripe@users.noreply.github.com>
2025-02-06 23:24:46 +0530

    Delete utils directory

--- utils/helper.py
+++
@@ -1,5 +0,0 @@
-def format_bold(text):
-    return f"**{text}**"
-
-def format_italic(text):
-    return f"_{text}_"
--- utils/validation.py
+++
@@ -1,2 +0,0 @@
-def validate_user_id(user_id):
-    return isinstance(user_id, int) and user_id > 0
